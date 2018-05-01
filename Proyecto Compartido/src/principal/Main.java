275590e79b2155b432966baf4eb943e5bad97b9d
Nation <Nation@Nation-PC>
2018-05-01 20:00:23 -0300

    Agrego propósito del proyecto

--- Proyecto Compartido/src/principal/Main.java
+++ Proyecto Compartido/src/principal/Main.java
@@ -1,6 +1,6 @@
-package principal;
+package principal; 
 
-public class Main {
+public class Main { //La idea es realizar un programa que con una cadena de texto dada, de la opcion al usuario de transformarlo todo en minuscula o mayuscula
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
