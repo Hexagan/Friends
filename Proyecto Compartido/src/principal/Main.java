d22c3d8dbaa161fb866cecb1c33574ad2433ef3d
Nation <Nation@Nation-PC>
2018-05-01 20:04:12 -0300

    Agrego variables, y joptionpane
    El usuario ahora coloca su texto en un cuadro de dialogo, por eso
    necesita la librería.

--- Proyecto Compartido/src/principal/Main.java
+++ Proyecto Compartido/src/principal/Main.java
@@ -1,10 +1,15 @@
-package principal; 
+package principal;
+
+import javax.swing.JOptionPane;
 
 public class Main { //La idea es realizar un programa que con una cadena de texto dada, de la opcion al usuario de transformarlo todo en minuscula o mayuscula
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String texto;
+		int decision;
+		
+		texto = JOptionPane.showInputDialog("Inserte un texto");
 	}
 
 }
