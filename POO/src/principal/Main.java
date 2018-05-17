ef553dfc85b93952427bc69badd9f7624d8a0e7e
Nation <Nation@Nation-PC>
2018-05-16 21:44:05 -0300

    Finish

--- POO/src/principal/Main.java
+++ POO/src/principal/Main.java
@@ -6,22 +6,26 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import javax.swing.JOptionPane;
+
 import java.io.File;
 
 public class Main {
 
 	public static void main(String[] args) {
-		File archivo = new File("C:\\Users\\Nation\\eclipse-workspace\\POO\\Texto.txt");
+		File archivo = new File("archivos\\Texto.txt");
 		BufferedReader br = null;
 		FileReader fr = null;
 		String linea;
+		String[] lineas;
 		String texto = "";
-		String palabra1, palabra2;
-		int i,j,k,mayor0,mayor1, mayor2, mayor3, mayor4, mayor5, mayor6, mayor7, mayor8, mayor9;
-	
+		String word1,word2;
+		int i,j;
+		
+		LinkedHashMap<String, Integer> Orden = new LinkedHashMap<String, Integer>();
 		
 		
 		try {
@@ -29,52 +33,54 @@ public class Main {
 			fr = new FileReader(archivo);
 			br = new BufferedReader(fr);
 			
-			while((linea = br.readLine()) != null) {
-				
-				System.out.println(linea);	
+			while((linea = br.readLine()) != null) {	
 				texto += linea;
-			
 			}
 			
 			texto = texto.toLowerCase(); 
 			texto = texto.replaceAll("\\.|\\,|\\;|\\?|\\¿|\\!|\\¡|\\s", "\n"); //Replace sin el \\W para que funcione con palabras tildadas y eñes
-		
-			ArrayList<String> Palabras = new ArrayList<>(Arrays.asList(texto.split("\\s")));
-
+			lineas = texto.split("\\s");
+			
+			ArrayList<String> Palabras = new ArrayList<>(Arrays.asList(lineas));
+			
 			Palabras.removeAll(Collections.singleton(""));
 			Palabras.removeAll(Collections.singleton(null));
-
-			int[] repetidas = new int[Palabras.size()];
+			
+		
+			for(i = 0; i < Palabras.size(); i++) {
+				Orden.put(Palabras.get(i), 0);
+			}
 			
 			for (i = 0; i < Palabras.size(); i++) {
-				palabra1 = Palabras.get(i);
-				
-				for (j = 0; j < Palabras.size(); j++) {
-					palabra2 = Palabras.get(j);
-						if(i == j) {
-							repetidas[j]++;
-						}
-						else
-						{
-							if(palabra1.equals(palabra2)) {
-								
-								repetidas[j]++;
-							}
+				word1=Palabras.get(i);
+				if(Orden.get(word1) == 0) {
+					
+					for (j = 0; j < Palabras.size(); j++) {
+						
+						word2=Palabras.get(j);
+						
+						if(word1.equals(word2) == true ) {
+							
+							Orden.put (word1,Orden.get(word1) + 1);	
 						}
 					}
-			
 				}
-			for (k = 0; k < repetidas.length; k++) {
-				if(repetidas[k]>mayor0)
 			}
-				
 			
-			for (int k : repetidas) {
-				System.out.println(k);	
-			}
-		}
-	
-		catch(IOException error){
+			Map<String, Integer> Resultado = new LinkedHashMap<>();
+			
+			Orden.entrySet().stream()
+			.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+			.forEachOrdered(x -> Resultado.put(x.getKey(), x.getValue()));
+			
+			
+			JOptionPane.showMessageDialog(null,"Las palabras mas utilizas en el texto ofrecido de mayor a menor son: ");
+			
+			for(Map.Entry<String,Integer> entry : Resultado.entrySet()){
+		        System.out.println(entry);
+		        }
+			
+		}catch(IOException error){
 			error.printStackTrace();
 		}
 		finally {
